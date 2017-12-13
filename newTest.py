0032006791c799e102f7b081e61bc28f9f370482
ninalutz <nlutz@mit.edu>
2017-12-13 00:25:24 -0500

    adding a new test thing

---
+++ newTest.py
@@ -0,0 +1,33 @@
+# Simple demo of of the WS2801/SPI-like addressable RGB LED lights.
+import time
+import RPi.GPIO as GPIO
+
+# Import the WS2801 module.
+import Adafruit_WS2801
+import Adafruit_GPIO.SPI as SPI
+
+
+# Configure the count of pixels:
+PIXEL_COUNT = 200
+
+# Alternatively specify a hardware SPI connection on /dev/spidev0.0:
+SPI_PORT   = 0
+SPI_DEVICE = 0
+pixels = Adafruit_WS2801.WS2801Pixels(PIXEL_COUNT, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE), gpio=GPIO)
+
+
+# Define rainbow cycle function to do a cycle of all hues.
+def rainbow_cycle_successive(pixels):
+    for i in range(pixels.count()):
+    	pixels.set_pixel(i, wheel(((i * 256 // pixels.count())) % 256) )
+    	pixels.show()
+
+if __name__ == "__main__":
+    # Clear all the pixels to turn them off.
+    print("HELLO")
+    pixels.clear()
+    pixels.show()  # Make sure to call show() after changing any pixels!
+
+    rainbow_cycle_successive(pixels)
+
+    print("GOODBYE")
