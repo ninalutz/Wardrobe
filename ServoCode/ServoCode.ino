13b207fa4204a5e4730331c5b15c3a0e89d2cb4b
ninalutz <nlutz@mit.edu>
2017-12-13 11:02:16 -0500

    Updating code

--- ServoCode/ServoCode.ino
+++ ServoCode/ServoCode.ino
@@ -5,9 +5,10 @@ Servo myservo;
 
 void setup() 
 { 
- Serial.begin(57600); // Start serial communication at 9600 bps
+  Serial.begin(57600); // Start serial communication at 9600 bps
   Serial.println("STARTED");
   pinMode(LED_BUILTIN, OUTPUT);
+  myservo.attach(9);
 } 
 
 void loop() {
@@ -19,12 +20,11 @@ void loop() {
     delay(1000);                       // wait for a second
 
     if(val == '1' && count < 5){
-          myservo.attach(9);
           myservo.write(90);  // set servo to mid-point
 //          digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
 //          delay(1000);                       // wait for a second
 //          digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
 //          delay(1000);                       // wait for a second
-         // count+=1;
+          count+=1;
       }
   } 
